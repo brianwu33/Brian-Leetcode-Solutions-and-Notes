6d55efee0504d65e5ab5b402606ab0de4a922c6e
Brian Wu <brianwu20020303@gmail.com>
2024-03-28 00:17:58 -0400

    Add One More Solution to Valid Anagram

--- Arrays and Hashing/valid-anagram.py
+++ Arrays and Hashing/valid-anagram.py
@@ -23,3 +23,17 @@ class Solution:
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+    
+#Solution 3 (Same algorithm as Solution 1, uses Counter)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+
+        return counter_s == counter_t
+    
+# These lines use the Counter class from the Python collections module to create dictionaries
+# that count the occurrences of each character in the strings s and t, respectively.
+# The Counter class is a specialized dictionary subclass designed for counting hashable objects.
+# Each key in the dictionary represents a unique character in the string, and the corresponding value represents the frequency of that character in the string.
